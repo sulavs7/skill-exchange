c8f20f26fdd87b887f433e91656c9ebab04f5589
Sulav Sapkota <sulavspkt7@gmail.com>
2024-12-06 17:27:41 +0545

    hello

--- models/userModel.js
+++ models/userModel.js
@@ -35,3 +35,52 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
+
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   role: {
+//     type: String,
+//     enum: ['tourist', 'local'], // Defines the type of user
+//     required: true,
+//   },
+//   category: {
+//     type: String,
+//     validate: {
+//       validator: function (value) {
+//         return this.role === 'local' ? value != null : true;
+//       },
+//       message: 'Category is required for locals',
+//     },
+//   },
+//   name: {
+//     type: String,
+//     required: function () {
+//       return this.role === 'local';
+//     },
+//   },
+//   phone: {
+//     type: String,
+//     required: function () {
+//       return this.role === 'local';
+//     },
+//   },
+//   description: {
+//     type: String,
+//     required: function () {
+//       return this.role === 'local';
+//     },
+//   },
+// });
+
+// module.exports = mongoose.model('User', userSchema);
